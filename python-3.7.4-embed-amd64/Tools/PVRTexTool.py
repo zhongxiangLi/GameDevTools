7b4185a07de49cadd4e7dca7d7ad30f670c8a140
陈鹏 <980097191@qq.com>
2019-07-11 18:19:56 +0800

    Update PVRTexTool.py

--- python-3.7.4-embed-amd64/Tools/PVRTexTool.py
+++ python-3.7.4-embed-amd64/Tools/PVRTexTool.py
@@ -17,7 +17,7 @@ def run():
 
     tmpExePath='"'+os.getcwd()+"\\Tools\\Imagination\\PowerVR_Graphics\\PowerVR_Tools\\PVRTexTool\\Start.bat"+'"'
     #print(tmpExePath)
-    Helper.ShowReadme(u"PVRTexTool取自Imagination PowerVR Tools and SDK 开发者工具包。\n更多工具请访问https://www.imgtec.com/developers/powervr-sdk-tools/installers/\n第一次使用请看说明文档\n"+os.getcwd()+u"\\Tools\\Imagination\\PowerVR_Graphics\\PowerVR_Tools\\PVRTexTool\\PVRTexTool使用说明.pdf")
+    Helper.ShowReadme(u"PVRTexTool取自Imagination PowerVR Tools and SDK 开发者工具包。\n更多工具请访问https://www.imgtec.com/developers/powervr-sdk-tools/installers/\n第一次使用请看说明文档\n"+os.getcwd()+u"\\Tools\\Imagination\\PowerVR_Graphics\\PowerVR_Tools\\PVRTexTool\\PVRTexTool使用说明.pdf\n\n类似软件有Mali Texture Compression Tool，和PVRTexTool 功能类似，不支持PVR格式,但是界面更美观，基于Java，所以安装包很大，没有放到GameDevTools中。\nhttps://developer.arm.com/tools-and-software/graphics-and-gaming/graphics-development-tools/mali-texture-compression-tool")
     # Helper.OpenOneToolDir("Win32DiskImager/Win32DiskImager/")
     os.system(tmpExePath)
     
